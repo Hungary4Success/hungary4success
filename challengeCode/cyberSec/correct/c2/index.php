ed19d557c07333128665e9aed4ee49ff0ebd43d3
Laszlo Nagy <laszlo.nagy@student.manchester.ac.uk>
2018-04-21 22:52:03 +0100

    Added hardcoded salt in cs/2 for easier checking

--- challengeCode/cyberSec/correct/c2/index.php
+++ challengeCode/cyberSec/correct/c2/index.php
@@ -1,16 +1,5 @@
 <?php
-//function to generate salt
-function generateSalt($max = 5) {
-    $characterList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*?";
-    $i = 0;
-    $salt = "";
-    while ($i < $max) {
-        $salt .= $characterList{mt_rand(0, (strlen($characterList) - 1))};
-        $i++;
-    }
-    return $salt;
-  }
-
+  $salt = "4235";
   $password = "2Nk8xQ0FLZuy";
   $hashedPassword = md5($salt . $password);//hash password using md5 with preceding salt
 ?>
